ca52e0f119a16c918147009bf09fa645bf49d26b
Vlad Tancev <vlad.tancev@gmail.com>
2022-08-29 18:38:45 +0300

    Add pdf upload and work with errors

--- extensions/onPreHandler.js
+++ extensions/onPreHandler.js
@@ -1,9 +1,7 @@
 module.exports = {
     events: 'onPreHandler',
     method: (request, h) => {
-        if (!request.payload) {
-            request.payload = {};
-        }
+        if (!request.payload) request.payload = {};
         return h.continue;
     }
 }
